c748a863aa00464858184a29ca1e52d5b73bbbef
yogjin <youngjin988@gmail.com>
2022-06-02 23:36:58 +0900

    feat: app 기본 틀 init

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,33 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View style={styles.walk}>
+        <Text>도보(자동): api이용해서 걸음 수 가져오기 -> 소모칼로리 보여주기</Text>
+      </View>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <View style={styles.calorie}>
+        <Text>섭취 칼로리: 수동입력</Text>
+      </View>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <View style={styles.weight}>
+        <Text>예상 체중변화: 섭취칼로리 - 기초대사량 + 도보</Text>
+      </View>
     </View>
   );
 }
@@ -20,13 +38,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
   separator: {
-    marginVertical: 30,
+    marginVertical: 10,
     height: 1,
     width: '80%',
   },
+  walk: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  calorie: { flex: 1, backgroundColor: 'blue' },
+  weight: { flex: 1, backgroundColor: 'green' },
 });
